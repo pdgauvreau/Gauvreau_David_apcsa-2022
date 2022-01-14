f6fb564b8a2c2f77f9f7b337c495f1fc8941be1a
pdgauvreau <pdgauvreau@gmail.com>
2022-01-14 09:29:44 -0800

    updated Unit 01

---
+++ Unit 01/src/StarsAndStripes.java
@@ -0,0 +1,50 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println(" ");
+	   out.println(" ");
+   }
+   
+   public void printASmallBox()
+   {	
+	   out.println("|----|");
+	   out.println("|    |");
+	   out.println("|____|");
+   }
+ 
+   public void printABigBox()
+   { 	
+	   out.println("|----------|");
+	   out.println("|          |");
+	   out.println("|          |");
+	   out.println("|          |");
+	   out.println("|__________|");
+   }   
+}
---
+++ Unit 01/src/StarsRunner.java
@@ -0,0 +1,47 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes draw = new StarsAndStripes();
+    
+      //call the methods needed to make the patterns on the word document
+     
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwoBlankLines();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printTwentyDashes();
+      draw.printTwentyStars();
+      draw.printASmallBox();
+      draw.printABigBox();
+      
+   }
+}
