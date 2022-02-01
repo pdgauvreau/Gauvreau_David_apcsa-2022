faefa404ec9b200a5dd877ece9558c5550317de0
pdgauvreau <pdgauvreau@gmail.com>
2022-02-01 09:23:32 -0800

    Update 06

--- Unit 06/src/CountPairs.java
+++ Unit 06/src/CountPairs.java
@@ -9,6 +9,11 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.substring(i).equals(str.substring(i + 1)));{
+				count = count + 1;
+			}
+		}
 		return count;
 	}
 }
