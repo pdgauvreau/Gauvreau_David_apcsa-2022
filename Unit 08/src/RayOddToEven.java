2a22d99b5958bf64380515d2477bd509fab85d78
pdgauvreau <pdgauvreau@gmail.com>
2022-02-08 09:29:14 -0800

    Unit 08 update

--- Unit 08/src/RayOddToEven.java
+++ Unit 08/src/RayOddToEven.java
@@ -7,6 +7,8 @@ public class RayOddToEven
 {
 	public static boolean go(int[] ray)
 	{
+		for (int i =0; i < ray.length; i++)
+			if (ray[i] % 2  = 1) 
 		return false;
 	}
 }
--- Unit 08/src/SumLastRunner.java
+++ Unit 08/src/SumLastRunner.java
@@ -7,6 +7,16 @@ public class SumLastRunner
 {
 	public static void main( String args[] )
 	{		
-		ray[-99,1,2,3,4,5,6,7,8,9,10,12345]
+		int ray[] = {-99,1,2,3,4,5,6,7,8,9,10,12345};
+		RaySumLast test = new RaySumLast();
+		System.out.println( test.go( new int[]{-99,1,2,3,4,5,6,7,8,9,10,12345}));
+		System.out.println( test.go( new int[]{10,9,8,7,6,5,4,3,2,1,-99}));
+		System.out.println( test.go( new int[]{10,20,30,40,50,-11818,40,30,20,10}));
+		System.out.println( test.go( new int[]{32767}));
+		System.out.println( test.go( new int[]{255,255}));
+		System.out.println( test.go( new int[]{9,10,-88,100,-555,1000}));
+		System.out.println( test.go( new int[]{10,10,10,11,456}));
+		System.out.println( test.go( new int[]{-111,1,2,3,9,11,20,30}));
+		System.out.println( test.go( new int[]{12,15,18,21,23,1000}));
 	}
 }
