c8879f2bd3176cdbd998bd34dd5bb257e092a4ca
pdgauvreau <pdgauvreau@gmail.com>
2022-02-10 09:18:29 -0800

    TriangleFive unit 07 push (late)

--- Unit 07/src/Unit7-Assignments/TriangleFive.java
+++ Unit 07/src/Unit7-Assignments/TriangleFive.java
@@ -9,34 +9,58 @@ public class TriangleFive
    private char letter;
    private int amount;
 
-	public TriangleFive()
-	{
-		for (int i = 0; i < amount; i++) {
-			print.ln (letter * (amount) + letter * (amount - 1))
-		}
-			
-	}
-
-	public TriangleFive(char c, int amt)
-	{
-		c = h;
-		amt = 0; 
-	}
-
-	public void setLetter(char c)
-	{
-		c = letter; 
-	}
-
-	public void setAmount(int amt)
-	{
-		amt = amount; 
-		
-	}
-
-	public String toString()
-	{
-		String output="";
-		return output;
-	}
+    public Triangle()
+    {
+        setLetter('a');
+        setAmount(0);
+    }
+    public Triangle(char c, int amt)
+    {
+        setLetter(c);
+        setAmount(amt);
+    }
+    public void setLetter(char c)
+    {
+        letter = c;
+    }
+    public void setAmount(int amt)
+    {
+        amount = amt;
+    }
+    public String drawTriangle() {
+        int num = 0;
+        int count2 = amount;
+        int count = 0;
+        int count3 = amount;
+        int count4 = amount;
+        int num2 = amount;
+        while (count2 > 0) {
+            count += count2;
+            count2--;
+        }
+        for (int d = 0; d < amount; d++) {
+        for (int i = 0; i < count3; i++) {
+            for (int j = 1; j <= count4;j++) {
+                if (num2 > num)
+                    out.print((char)(letter + num));
+                if (j == count4 &&count4 != 0) {
+                    num++;
+                    count4--;
+                    out.print(" ");
+                }
+            }
+        count--;
+        }
+        num2--;
+        count4 = amount;
+        num = 0;
+        out.println();
+        }
+        String output = "";
+        return output;
+    }
+    public String toString()
+    {
+        return drawTriangle();
+    }
 }
--- Unit 08/src/RayOddToEven.java
+++ Unit 08/src/RayOddToEven.java
@@ -7,8 +7,10 @@ public class RayOddToEven
 {
 	public static boolean go(int[] ray)
 	{
-		for (int i =0; i < ray.length; i++)
-			if (ray[i] % 2  = 1) 
-		return false;
+		for (int i = 0; i < ray.length; i++)
+			if (!(ray[i] % 2  == 1)) return "-1";
+			else for (int j = i; j < ray.length -1; j++)
+				if (ray[j] % 2 == 0) return  j - i + " "; 
+		
 	}
 }
