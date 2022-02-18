17ebf7d2dc4e16d94b3d55bf0adf74a073fa7daf
pdgauvreau <pdgauvreau@gmail.com>
2022-02-18 14:40:42 -0800

    Unit 09 Update

--- Unit 09/src/DownRunner.java
+++ Unit 09/src/DownRunner.java
@@ -8,6 +8,6 @@ public class DownRunner
 	public static void main( String args[] )
 	{
 		ArrayList<Integer> ray = new ArrayList<Integer>();
-		System.out.println(go.ray(99,1,2,3,4,5,6,7,8,9,10,12345));
+		System.out.println(go(99,1,2,3,4,5,6,7,8,9,10,12345));
 	}
 }
