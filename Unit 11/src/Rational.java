aa031bbc7ea2f7d4d796848a37fa8af54d4eb9d8
pdgauvreau <pdgauvreau@gmail.com>
2022-03-02 09:30:23 -0800

    Update unit 11

--- Unit 11/src/Rational.java
+++ Unit 11/src/Rational.java
@@ -7,19 +7,32 @@ import static java.lang.System.*;
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-
+ private int rOne;
+ private int rTwo;
 	//write two constructors
-
-
+public void rational (int rOne) {
+	rOne = 0;
+}
+ 
+public void number (int rTwo) {
+	rTwo = 0;
+}
 	//write a setRational method
+public void setRational(int num1, int den1) {
 	
-
+}
 	//write  a set method for numerator and denominator
-
+public void setFraction(int num1, int den1, int num2, int den2) {
+	num1 = 0;
+	den1 = 0;
+	num2 = 0;
+	den2 = 0;
+}
 	
 	public void add(Rational  other)
 	{
 		//num1/den1 + num2/den2 
+	
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
 		
