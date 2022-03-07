5effcb0be794a21a3a7a83b74f1e30f61d6f7f58
pdgauvreau <pdgauvreau@gmail.com>
2022-03-07 14:58:10 -0800

    Unit 11 Update ToyStore

--- Unit 11/src/ToyRunner.java
+++ Unit 11/src/ToyRunner.java
@@ -8,5 +8,12 @@ public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Toy test1 = new Toy("SORRY");
+		test1.setCount(1);
+		System.out.println(test1);
+		
+		Toy test2 = new Toy(GI Joe");"
+		test2.setCount(5);;
+		System.out.println(test2);
 	}
 }
--- Unit 11/src/ToyStore.java
+++ Unit 11/src/ToyStore.java
@@ -10,36 +10,87 @@ import static java.lang.System.*;
 
 public class ToyStore
 {
-	private String name;
-	private int count;
-	
 	private ArrayList<Toy> toyList;
 
 	public ToyStore()
 	{
-		toyList = new Array
+		loadToys("");
+	}
+	
+	public ToyStore(String toys)
+	{
+		loadToys(toys);
 	}
 
-	public void loadToys( String toys )
+	public void loadToys(String toys)
 	{
+		toyList = new ArrayList<Toy>();
+		for (String newToy : toys.split(" "))
+		{
+			boolean ToyInList = false;
+			for (Toy toy: toyList)
+			{
+				if (newToy.equals(toy.getName()))
+				{
+					toy.setCount(toy.getCount() + 1);
+					ToyInList = true;
+					
+				}
+			}
+			
+			if (ToyInList == false)
+			{
+				toyList.add(new Toy(newToy));
+			}
+		}
 	}
   
-  	public Toy getThatToy( String nm )
+  	public Toy getThatToy(String nm)
   	{
+  		for (Toy fetchToy: toyList)
+  		{
+  			if (fetchToy.getName().equals(nm))
+  			{
+  				return fetchToy;
+  			}
+  		}
   		return null;
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+  		Toy mostFrequent = toyList.get(0);
+  		
+  		for (int index = 0; index < toyList.size(); index++)
+  		{
+  			if (toyList.get(index).getCount() > mostFrequent.getCount())
+  			{
+  				mostFrequent = toyList.get(index);
+  			}
+  		}
+  		return mostFrequent.getName();
   	}  
   
   	public void sortToysByCount()
   	{
+  		boolean sorted = false;
+  		while (sorted == false)
+  		{
+  			sorted = true;
+  			for (int i = 0; i < toyList.size() - 1; i++)
+  			{
+  				if (toyList.get(i).getCount() < toyList.get(i + 1).getCount())
+  				{
+  					toyList.add(i, toyList.get(i + 1));
+  					toyList.remove(i + 2);
+  					sorted = false;
+  				}
+  			}
+  		}
   	}  
   	  
 	public String toString()
 	{
-	   return "";
+	   return Arrays.toString(toyList.toArray());
 	}
 }
