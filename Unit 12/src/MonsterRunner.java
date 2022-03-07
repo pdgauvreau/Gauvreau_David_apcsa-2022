fc5144a6a502835a7863bf5df361b7b88dfe9429
pdgauvreau <pdgauvreau@gmail.com>
2022-03-07 08:46:49 -0800

    Unit 12 update

--- Unit 12/src/MonsterRunner.java
+++ Unit 12/src/MonsterRunner.java
@@ -12,10 +12,22 @@ public class MonsterRunner
 		Scanner keyboard = new Scanner(System.in);
 		
 		//ask for name and size
+		System.out.println ("Enter 1st monster's name : ");
+		String name1 = keyboard.next();
+		System.out.println ("Enter 1st monster's size : ");
+		int size1 = keyboard.nextInt();
 		
+
 		//instantiate monster one
+		size1 = 0; 
+	
 		
 		//ask for name and size
+		System.out.println ("Enter 2nd monster's name : ");
+		String name2 = keyboard.next();
+		System.out.println ("Enter 2nd monster's size : ");
+		int size2 = keyboard.nextInt();
+		
 		
 		//instantiate monster two
 	}
