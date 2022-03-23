f08fecf48aaaa2dfd235b64d7e93dc1eb751c4bf
pdgauvreau <pdgauvreau@gmail.com>
2022-03-23 13:27:51 -0700

    Unit 14 update

--- Unit 14/src/Grid.java
+++ Unit 14/src/Grid.java
@@ -9,12 +9,33 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[rows][cols];
+		
+		for (int row = 0; row < grid.length; row++)
+		{
+			for (int col = 0; col < grid[row].length; col++)
+			{
+				grid[row][col] = vals[(int)(Math.random()*vals.length)];
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		int[] counts = new int[vals.length];
+		
+		for (int i = 0; i < vals.length; i++)
+		{
+			counts[i] = countVals(vals[i]);
+		}
+		int maxIndex = 0;
+		
+		for (int counter = 0; counter < vals.length; counter++)
+		{
+			if (counts[counter] > counts[maxIndex]) maxIndex = counter;
+		}
+		return vals[maxIndex];
 	}
 
 	//returns a count of how many times val occurs in the matrix
